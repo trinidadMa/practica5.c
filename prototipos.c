c91d6be2bdd070a932d2bd47d21e8a0a22ec77b5
trinidadMa <125688275+trinidadMa@users.noreply.github.com>
2023-03-20 21:15:36 -0600

    Create prototipos.c

---
+++ prototipos.c
@@ -0,0 +1,23 @@
+#define  misfunciones.h
+
+int sumar_enteros;
+int numero_a;
+int numero_b;
+int restar_enteros;
+int numero_a;
+int numero_b;
+
+int sumar_enteros;
+int numero_a,int numero_b
+{
+int suma;
+suma=numero_a+numero_b;
+return suma;
+}
+int restar_enteros
+int numero_a,int numero_b
+{
+int resta;
+resta=numero_a-numero_b;
+return resta;
+}
